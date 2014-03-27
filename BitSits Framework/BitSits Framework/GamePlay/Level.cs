51f83025f569c28e7a0150652358738af441bb6d
Shubhajit Saha <shubhajit@iitb.ac.in>
2014-03-27 16:03:50 +0530

    First Submit

---
+++ BitSits Framework/BitSits Framework/GamePlay/Level.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace BitSits_Framework
+{
+    class Level : IDisposable
+    {
+        #region Fields
+
+
+        public int Score { get; private set; }
+
+        public bool IsLevelUp { get; private set; }
+        public bool ReloadLevel { get; private set; }
+        int levelIndex;
+
+        GameContent gameContent;
+
+        BasicEffect basicEffect;
+        //Render render = new Render();
+
+
+        #endregion
+
+        #region Initialization
+
+
+        public Level(ScreenManager screenManager, int levelIndex)
+        {
+            this.gameContent = screenManager.GameContent;
+            this.levelIndex = levelIndex;
+
+            basicEffect = new BasicEffect(screenManager.GraphicsDevice);
+            basicEffect.VertexColorEnabled = true;
+
+            //Render._device = screenManager.GraphicsDevice;
+            //Render._batch = screenManager.SpriteBatch;
+            //Render._font = debugFont;
+        }
+
+
+        private void LoadTiles(int levelIndex)
+        {
+            // Load the level and ensure all of the lines are the same length.
+            int width;
+            List<string> lines = new List<string>();
+            lines = gameContent.content.Load<List<string>>("Levels/level" + levelIndex.ToString("0"));
+
+            width = lines[0].Length;
+            // Loop over every tile position,
+            for (int y = 0; y < lines.Count; ++y)
+            {
+                if (lines[y].Length != width)
+                    throw new Exception(String.Format(
+                        "The length of line {0} is different from all preceeding lines.", lines.Count));
+
+                for (int x = 0; x < lines[0].Length; ++x)
+                {
+                    // to load each tile.
+                    char tileType = lines[y][x];
+                    LoadTile(tileType, x, y);
+                }
+            }
+        }
+
+
+        private void LoadTile(char tileType, int x, int y)
+        {
+            switch (tileType)
+            {
+                case '.': break;
+
+                // Unknown tile type character
+                default:
+                    throw new NotSupportedException(String.Format(
+                        "Unsupported tile type character '{0}' at position {1}, {2}.", tileType, x, y));
+            }
+        }
+
+
+        public void Dispose() { }
+
+
+        #endregion
+
+        #region Update and HandleInput
+
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+
+        public void HandleInput(InputState input, int playerIndex)
+        {
+            if (input.IsMouseLeftButtonClick()) IsLevelUp = true;
+            if (input.IsMouseRightButtonClick()) ReloadLevel = true;
+        }
+
+
+        #endregion
+
+        #region Draw
+
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(gameContent.gameFont, "Level : " + (levelIndex + 1),
+                new Vector2(400, 20), Color.White);
+
+            //basicEffect.Techniques[0].Passes[0].Apply();
+            //render.FinishDrawShapes();
+            //render.FinishDrawString();
+        }
+
+
+        #endregion
+    }
+}
---
+++ BitSits Framework/BitSits Framework/ScreenManager/GameContent.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using GameDataLibrary;
+
+#if WINDOWS_PHONE
+using System.Windows;
+#endif
+
+namespace BitSits_Framework
+{
+    /// <summary>
+    /// All the Contents of the Game is loaded and stored here
+    /// so that all other screen can copy from here
+    /// </summary>
+    public class GameContent
+    {
+        public ContentManager content;
+        
+        public Random random = new Random();
+
+        public readonly float b2Scale = 30;
+
+        // Textures
+        public Texture2D blank;
+        public Texture2D menuBackground, mainMenuTitle;
+
+        // Fonts
+        public SpriteFont debugFont, gameFont;
+        public int gameFontSize;
+        
+
+        /// <summary>
+        /// Load GameContents
+        /// </summary>
+        public GameContent(GameComponent screenManager)
+        {
+            content = screenManager.Game.Content;
+
+            blank = content.Load<Texture2D>("Graphics/blank");
+            menuBackground = content.Load<Texture2D>("Graphics/menuBackground");
+
+            mainMenuTitle = content.Load<Texture2D>("Graphics/BitSitsGamesLogo");
+
+            debugFont = content.Load<SpriteFont>("Fonts/debugFont");
+
+            gameFontSize = 60;
+            gameFont = content.Load<SpriteFont>("Fonts/chunky" + gameFontSize.ToString());
+
+            MediaPlayer.IsRepeating = true;
+
+#if DEBUG
+            MediaPlayer.Volume = .4f; SoundEffect.MasterVolume = .4f;
+#else
+            if (BitSitsGames.Settings.MusicEnabled) PlayMusic();
+
+            if (BitSitsGames.Settings.SoundEnabled) SoundEffect.MasterVolume = 1;
+            else SoundEffect.MasterVolume = 0;
+#endif
+
+            // Initialize audio objects.
+            //audioEngine = new AudioEngine("Content/Audio/Audio.xgs");
+            //soundBank = new SoundBank(audioEngine, "Content/Audio/Sound Bank.xsb");
+            //waveBank = new WaveBank(audioEngine, "Content/Audio/Wave Bank.xwb");
+            //soundBank.GetCue("music").Play();
+
+
+            //Thread.Sleep(1000);
+
+            // once the load has finished, we use ResetElapsedTime to tell the game's
+            // timing mechanism that we have just finished a very long frame, and that
+            // it should not try to catch up.
+            screenManager.Game.ResetElapsedTime();
+        }
+
+        public void PlayMusic()
+        {
+            if (MediaPlayer.GameHasControl)
+            {
+                if (MediaPlayer.State == MediaState.Paused)
+                {
+                    MediaPlayer.Resume();
+
+                    return;
+                }
+            }
+            else if (MediaPlayer.State == MediaState.Playing)
+            {
+#if WINDOWS_PHONE
+                MessageBoxResult Choice;
+
+                Choice = MessageBox.Show("Media is currently playing, do you want to stop it?",
+                    "Stop Player", MessageBoxButton.OKCancel);
+
+                if (Choice == MessageBoxResult.OK) MediaPlayer.Pause();
+                else
+                {
+                    BitSitsGames.Settings.MusicEnabled = false;
+                    return;
+                }
+#endif
+            }
+
+            //MediaPlayer.Play(content.Load<Song>("Audio/Back to old school"));
+            MediaPlayer.IsRepeating = true;
+        }
+
+        /// <summary>
+        /// Unload GameContents
+        /// </summary>
+        public void UnloadContent() { content.Unload(); }
+    }
+}
